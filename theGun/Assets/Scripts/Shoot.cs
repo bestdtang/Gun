360d6df097affa32f0866fa8926b4a8811d4276d
bestdtang <51592725+bestdtang@users.noreply.github.com>
2019-07-09 16:29:55 +0800

    Add shooting mechanic

---
+++ theGun/Assets/Scripts/Shoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera _camera;
+    private Vector3 rayOrigin;
+    LayerMask shootableMask;
+    void Start()
+    {
+        //shootableMask = LayerMask.GetMask("Shootable");
+        //layerMask = ~layerMask;
+    }
+
+    public void ShootBullet()
+    {
+        RaycastHit hitObj;
+        rayOrigin = _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+        Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hitObj, 400f);//, shootableMask);
+        if (hitObj.transform.tag == "Target")
+        {
+            Debug.Log("hit");
+            Destroy(hitObj.transform.gameObject);
+        }
+        else
+        {
+            Debug.Log("hitwall");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(_camera.transform.position, _camera.transform.forward * 400f, Color.white);
+    }
+}
