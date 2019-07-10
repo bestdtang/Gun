68b45618998867be64833833f9370dfd012a53e5
bestdtang <51592725+bestdtang@users.noreply.github.com>
2019-07-10 17:59:22 +0800

    add particles

---
+++ theGun/Assets/Scripts/DeleteHole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteHole : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float destoryTime;
+    private float startTime;
+    void Start()
+    {
+        startTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime > destoryTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- theGun/Assets/Scripts/Shoot.cs
+++ theGun/Assets/Scripts/Shoot.cs
@@ -6,12 +6,16 @@ public class Shoot : MonoBehaviour
 {
     // Start is called before the first frame update
     public Camera _camera;
+    public GameObject holePre;
+    public Transform _holeParent;
     private Vector3 rayOrigin;
     LayerMask shootableMask;
+    private Animator cameraShakeAnim;
     void Start()
     {
         //shootableMask = LayerMask.GetMask("Shootable");
         //layerMask = ~layerMask;
+        cameraShakeAnim = _camera.transform.parent.parent.gameObject.GetComponent<Animator>();
     }
 
     public void ShootBullet()
@@ -19,14 +23,24 @@ public class Shoot : MonoBehaviour
         RaycastHit hitObj;
         rayOrigin = _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
         Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hitObj, 400f);//, shootableMask);
+        cameraShakeAnim.SetTrigger("shake");
+
         if (hitObj.transform.tag == "Target")
         {
-            Debug.Log("hit");
-            Destroy(hitObj.transform.gameObject);
+            //Debug.Log("hit");
+            //Destroy(hitObj.transform.gameObject);
+            GameObject parent = hitObj.transform.parent.gameObject;
+            Animator tarAnim = parent.GetComponent<Animator>();
+            tarAnim.SetTrigger("shoot");
         }
         else
         {
-            Debug.Log("hitwall");
+            Vector3 newPosit = hitObj.point + (_camera.transform.position - hitObj.point).normalized * 0.01f;
+            GameObject newHole = Instantiate(holePre, newPosit, Quaternion.FromToRotation(Vector3.up, hitObj.normal));
+
+            //newHole.transform.position = newPosit;
+            //newHole.transform.LookAt(_camera.transform);
+            newHole.transform.SetParent(_holeParent);
         }
 
     }
